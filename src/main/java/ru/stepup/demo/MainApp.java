54e6b3590fffd376751e750fe30d931202adc4bd
Angelina Borgoyakova <a.borgoiakova@bars.group>
2024-07-01 15:19:46 +0700

    commit for branch task2

--- src/main/java/ru/stepup/demo/MainApp.java
+++ src/main/java/ru/stepup/demo/MainApp.java
@@ -3,6 +3,7 @@ package ru.stepup.demo;
 public class MainApp {
     public static void main(String[] args){
         System.out.println("Hello world!");
-        System.out.println("111111");
+        System.out.println("----");
+        System.out.println("111112");
     }
 }
